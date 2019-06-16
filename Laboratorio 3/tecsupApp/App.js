df1285c4371a1bcaefed9cf5b70fd882dabb252b
WillyBaldeon <alex.memito@hotmail.com>
2019-06-15 23:30:18 -0500

    Laboratorio 3 completo.

---
+++ Laboratorio 3/tecsupApp/App.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('./logo.png')}
+        />
+
+        <Text style={styles.welcome}>!!Bienvenido Willy!!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
